7f12cdfa36a37cb82ab90d6aa0d410a34807657b
matt321An <mattanghel@yahoo.ro>
2022-02-15 08:55:39 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,39 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            // Create the db if it doesn't exists
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var context = services.GetRequiredService<StoreContext>();
+
+                    // Here we create the db
+                    await context.Database.MigrateAsync();
+
+                    // Here we seed the data
+                    await StoreContextSeed.SeedAsync(context, loggerFactory);
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error occured during migration");
+                }
+            }
+
+            // Start the application
+            host.Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        public Task<Product> GetProductByIdAsync(int id);
+        public Task<IReadOnlyList<Product>> GetProductsAsync();
+        public Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+        public Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
+
+    }
+}
