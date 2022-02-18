885bb81debdfe4dfdfb9b566d6b2ab5c3e0b3df9
matt321An <mattanghel@yahoo.ro>
2022-02-18 09:06:26 +0200

    End of section 14

--- client/src/app/core/services/busy.service.ts
+++ client/src/app/core/services/busy.service.ts
@@ -13,14 +13,14 @@ export class BusyService {
     this.busyRequestCount++;
     this.spinnerService.show(undefined, {
       type: 'timer',
-      bdColor: 'rgba(255, 255, 255, 0.7)',
+      bdColor: 'rgba(255,255,255,0.7)',
       color: '#333333'
     });
   }
 
   idle() {
     this.busyRequestCount--;
-    if(this.busyRequestCount <= 0){
+    if (this.busyRequestCount <= 0) {
       this.busyRequestCount = 0;
       this.spinnerService.hide();
     }
